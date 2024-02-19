23ea10a10ecb80da2ab5a796faf90a2e20a08f5d
Ferdinand Calo <fexcalo@gmail.com>
2024-02-19 09:47:58 +0100

    Update ClientProxy.java

--- src/main/java/bidahochi/foxdrives/util/ClientProxy.java
+++ src/main/java/bidahochi/foxdrives/util/ClientProxy.java
@@ -1,12 +1,17 @@
 package bidahochi.foxdrives.util;
 
 
+import bidahochi.foxdrives.entities.EntityCar;
 import bidahochi.foxdrives.entities.EntityCarChest;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import org.lwjgl.input.Keyboard;
 
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
 
     public static final RenderCar transportRenderer = new RenderCar();
@@ -28,4 +33,32 @@ public class ClientProxy extends CommonProxy {
         }
         return null;
     }
+
+    public static final net.minecraft.client.renderer.entity.RenderPlayer playerRender = new net.minecraft.client.renderer.entity.RenderPlayer(){
+        EntityCar t;
+        @Override
+        public void doRender(net.minecraft.client.entity.AbstractClientPlayer p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_){
+            if (p_76986_1_.ridingEntity instanceof EntityCar) {
+                t=(EntityCar) p_76986_1_.ridingEntity;
+                org.lwjgl.opengl.GL11.glPushMatrix();
+                org.lwjgl.opengl.GL11.glScalef(t.getRiderScale(), t.getRiderScale(), t.getRiderScale());
+                super.doRender(p_76986_1_, p_76986_2_, p_76986_4_, p_76986_6_, p_76986_8_, p_76986_9_);
+                org.lwjgl.opengl.GL11.glPopMatrix();
+
+            } else {
+                super.doRender(p_76986_1_, p_76986_2_, p_76986_4_, p_76986_6_, p_76986_8_, p_76986_9_);
+            }
+        }
+    };
+
+    @Override
+    public void registerCarRenderer(Class<? extends EntityCar> clazz){
+        RenderingRegistry.registerEntityRenderingHandler(clazz, transportRenderer);
+    }
+
+    @Override
+    public void registerPlayerScaler(){
+        RenderingRegistry.registerEntityRenderingHandler(EntityPlayer.class, playerRender);
+    }
+
 }
